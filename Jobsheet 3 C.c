29ef13d31ffd088bc884f0b3ed2b25da1fa46846
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 21:00:26 +0700

    Create Jobsheet 3 C.c
    buatlah program untuk menampilkan nama,nim dan umur dengan menerapkan variabel lokal dan global dan input data secara dinamis

---
+++ Jobsheet 3 C.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int nim, umur;
+
+int main(){
+char nama[100];
+
+printf ("Nama : ");
+	gets(nama);
+	
+printf ("NIM : ");
+	scanf("%s", &nim);
+	
+printf("Umur : ");
+	scanf("%d", &umur);
+
+
+}
